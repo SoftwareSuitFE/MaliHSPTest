ad7cac503aeb03606b96657f5aac1eb21d11e48c
Mehmet Ali Yıldız <mehmetali.yildiz@softwaresuit.com>
2025-03-31 23:49:08 +0300

    responsive - header bottom

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,14 +4,17 @@
 // import Header from "@/components/layout/header";
 // import Filters from "@/components/search/filters";
 // import HotelList from "@/components/search/hotelList";
+// import { useLanguage } from "@/hooks/useLanguage";
 
 // export default function SearchPage(): JSX.Element {
+//   const { t } = useLanguage();
+
 //   return (
 //     <main className="min-h-screen bg-white">
 //       <Header />
 //       <section className="container mx-auto mt-8 px-4">
 //         <div className="max-w-[1400px] mx-auto">
-//           <div className="grid grid-cols-1 lg:grid-cols-[270px_minmax(0,1fr)] gap-0">
+//           <div className="grid grid-cols-1 lg:grid-cols-[270px_minmax(0,1fr)] gap-4">
 //             <div className="p-6">
 //               <Filters />
 //             </div>
@@ -41,15 +44,15 @@ export default function SearchPage(): JSX.Element {
   return (
     <main className="min-h-screen bg-white">
       <Header />
+      {/* Container yapısını Header ile aynı şekilde oluşturduk */}
       <section className="container mx-auto mt-8 px-4">
-        <div className="max-w-[1400px] mx-auto">
-          <div className="grid grid-cols-1 lg:grid-cols-[270px_minmax(0,1fr)] gap-4">
-            <div className="p-6">
-              <Filters />
-            </div>
-            <div className="p-6">
-              <HotelList />
-            </div>
+        {/* max-width belirtmiyoruz, container ile sağlanıyor */}
+        <div className="grid grid-cols-1 lg:grid-cols-[270px_minmax(0,1fr)] gap-4">
+          <div className="p-0">
+            <Filters />
+          </div>
+          <div className="p-0">
+            <HotelList />
           </div>
         </div>
       </section>
