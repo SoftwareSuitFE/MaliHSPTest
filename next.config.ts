f09335f832ce4d1063aaa1a540623ef0c735db1a
Mehmet Ali Yıldız <mehmetali.yildiz@softwaresuit.com>
2025-04-02 04:40:11 +0300

    bugs fixed

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["images.unsplash.com", "source.unsplash.com"],
-//   },
-//   // For Ant Design to work properly with Next.js + App Router
-//   transpilePackages: ["@ant-design"],
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
